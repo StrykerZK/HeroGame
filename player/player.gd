4d84dca61964f3ad324ddeaa9a3fe15d0aeaf328
Zwe <zwe.mk24@gmail.com>
2025-09-03 01:31:47 -0700

    laser eyes; added kenny's suburban models

--- player/player.gd
+++ player/player.gd
@@ -8,7 +8,7 @@ var mouse_sensitivity := base_mouse_sensitivity
 @export_group("Aiming")
 @export var aim_fov := 40.0
 @export var aim_speed := 10.0
-@export var aim_mouse_sensitivity_mult := 0.5
+@export var aim_mouse_sensitivity_mult := 0.3
 var is_aiming := false
 
 @export_group("Movement")
@@ -45,10 +45,17 @@ var current_health := base_health
 @export var defense := 4.0
 @export var speed_modifier := 1.0 # Multiplier for external effects (buffs/debuffs)
 
+@export_group("Miscellaneous")
+@export_subgroup("Lasers")
+var laser_damage := global_dmg_scale * 5.0
+@export var laser_range := 10000.0
+@export var laser_slow_modifier := 0.25
+var firing_laser := false
+
 # -- States --
 enum State {
 	GROUNDED, RUNNING, SNEAKING, CHARGING_JUMP, LANDING,
-	IN_AIR, FLIGHT, SUPER_FLIGHT
+	IN_AIR, FLIGHT, SUPER_FLIGHT,
 	}
 var current_state = State.GROUNDED
 var last_state = State.GROUNDED
@@ -70,18 +77,22 @@ var click_count := 0
 @onready var _camera: Camera3D = %Camera
 @onready var _camera_spring: SpringArm3D = %CameraSpring
 @onready var _shoulder_pivot: Node3D = %ShoulderPivot
+@onready var _eye_l: Node3D = %EyeL
+@onready var _eye_r: Node3D = %EyeR
+@onready var _laser_l: Node3D = %LaserBeamL
+@onready var _laser_r: Node3D = %LaserBeamR
+@onready var _aim_raycast: RayCast3D = %AimRayCast
 @onready var _camera_animation: AnimationPlayer = %CameraPivotAnimation
 @onready var _stickman := %Bob
 @onready var double_click_timer := %DoubleClickTimer
 @onready var step_up_cast := %StepUpCast
 @onready var sonic_boom_vfx := %SonicBoomFX
-@onready var fx_player := %FXPlayer
+@onready var boom_sfx := %BoomSFX
+@onready var laser_sfx := %LaserSFX
 
-# -- Scene assignment --
+# -- Scene assignment --  
 @export_group("Scenes")
 @export var world_environment_node: WorldEnvironment
-@onready var poof_effect_scene: PackedScene = preload("res://assets/vfx/poof.tscn")
-@onready var projectile_scene: PackedScene = preload("res://temp_scenes/test_projectile.tscn")
 
 func _ready():
 	if is_instance_valid(LODManager):
@@ -89,6 +100,9 @@ func _ready():
 	
 	if is_multiplayer_authority():
 		_setup_local_player_fog()
+	
+	_laser_l.visible = false
+	_laser_r.visible = false
 	
 	_camera.fov = base_fov
 	_gravity = base_gravity
@@ -114,7 +128,7 @@ func _input(event):
 	if event.is_action_pressed("sprint"):
 		match current_state:
 			State.FLIGHT:
-				if air_brake == true: return
+				if air_brake or firing_laser: return
 				current_state = State.SUPER_FLIGHT
 				air_brake = false
 				mouse_sensitivity *= 0.4
@@ -135,7 +149,7 @@ func _input(event):
 	if event.is_action_pressed("crouch"):
 		match current_state:
 			State.FLIGHT:
-				velocity.y = -ascend_descend_speed
+				velocity.y = -ascend_descend_speed * speed_modifier
 			State.GROUNDED:
 				current_state = State.SNEAKING
 	if event.is_action_released("crouch"):
@@ -159,19 +173,27 @@ func _input(event):
 					click_count = 0
 					return
 				if current_state == State.FLIGHT:
-					velocity.y = ascend_descend_speed
+					velocity.y = ascend_descend_speed * speed_modifier
 	if event.is_action_released("jump"):
 		if current_state == State.FLIGHT:
 			if velocity.y > 0.0:
 				velocity.y = 0.0
 	
 	# Skill 1 Logic (Default 'Q')
-	if event.is_action_pressed("skill_1") and %SpeedTimer.is_stopped():
-		speed_up(15.0)
+	if event.is_action_pressed("skill_1"):
+		pass
 	
 	# Skill 2 Logic (Default 'E')
 	if event.is_action_pressed("skill_2"):
-		spawn_projectile()
+		match current_state:
+			State.SUPER_FLIGHT, State.CHARGING_JUMP, State.LANDING:
+				return
+			_:
+				firing_laser = true
+	if event.is_action_released("skill_2"):
+		if firing_laser == true:
+			firing_laser = false
+			reset_speed_modifier()
 	
 	# Shoulder Switch Logic (Default 'Middle Mouse')
 	if event.is_action_pressed("switch_shoulders"):
@@ -229,6 +251,7 @@ func _physics_process(delta):
 	if can_move:
 		move_and_slide()
 	
+	# Dynamic Camera
 	handle_rotation(delta)
 	handle_camera_zoom(delta)
 	
@@ -237,6 +260,14 @@ func _physics_process(delta):
 		_camera_spring.spring_length = lerp(_camera_spring.spring_length, 2.0, delta * 5.0)
 	elif _camera_spring.spring_length != 4.0:
 		_camera_spring.spring_length = lerp(_camera_spring.spring_length, 4.0, delta * 10.0)
+	
+	# Laser Logic
+	if firing_laser:
+		laser_eyes(delta)
+	if _laser_l.visible != firing_laser: _laser_l.visible = firing_laser
+	if _laser_r.visible != firing_laser: _laser_r.visible = firing_laser
+	if _aim_raycast.enabled != firing_laser: _aim_raycast.enabled = firing_laser
+	if laser_sfx.playing != firing_laser: laser_sfx.playing = firing_laser
 
 	
 	# Stickman Animations
@@ -320,7 +351,10 @@ func ground_movement(delta):
 			global_position.y += step_height + 0.05
 			velocity.y = 0
 	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor() and !firing_laser:
+		if is_aiming:
+			is_aiming = false
+			mouse_sensitivity = base_mouse_sensitivity
 		current_state = State.CHARGING_JUMP
 		velocity = Vector3.ZERO
 		can_move = false
@@ -333,21 +367,13 @@ func ground_movement(delta):
 			current_jump_charge = min(current_jump_charge, 1.0)
 		if Input.is_action_just_released("jump"):
 			current_state = State.IN_AIR
-			if current_jump_charge >= 0.5: can_fly = true
+			if current_jump_charge >= 0.3: can_fly = true
 			var jump_impulse = lerp(min_jump_impulse, max_jump_impulse, current_jump_charge)
 			velocity.y = jump_impulse
-			
-			# Poof effect
-			var poof = poof_effect_scene.instantiate()
-			get_tree().root.add_child(poof)
-			poof.global_position = %Marker3D.global_position
-			poof.emitting = true
-			poof.finished.connect(poof.queue_free)
 			
 			# Reset Charging
 			current_jump_charge = 0.0
 			can_move = true
-
 
 func flight_movement(delta):
 	var raw_input := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
@@ -433,7 +459,7 @@ func landing_movement(delta):
 func handle_rotation(delta):
 	var target_basis: Basis
 
-	if is_aiming:
+	if is_aiming or firing_laser:
 		# If aiming, align the character with the camera's horizontal rotation
 		var cam_y_rotation = _shoulder_pivot.global_rotation.y
 		var target_quat = Quaternion(Vector3.UP, cam_y_rotation)
@@ -447,9 +473,6 @@ func handle_rotation(delta):
 			target_basis = Transform3D().looking_at(-_last_movement_direction, Vector3.UP).basis
 			_stickman.global_transform.basis = _stickman.global_transform.basis.orthonormalized().slerp(target_basis, rotation_speed * delta)
 
-	# Sync spawn pivot to the stickman's rotation
-	$SpawnPivot.global_transform.basis = _stickman.global_transform.basis
-
 func handle_camera_zoom(delta):
 	var target_fov: float
 	if is_aiming:
@@ -462,12 +485,38 @@ func handle_camera_zoom(delta):
 	# Smoothly interpolate to the target FOV
 	_camera.fov = lerp(_camera.fov, target_fov, aim_speed * delta)
 
-func speed_up(new_mult):
-	var temp_mult = speed_mult
-	speed_mult = new_mult
-	%SpeedTimer.start()
-	await %SpeedTimer.timeout
-	speed_mult = temp_mult
+func laser_eyes(delta):
+	set_speed_modifier(laser_slow_modifier) # Slow down player
+	# Targeting Logic
+	var target_pos = _aim_raycast.get_collision_point()
+	# If the raycast doesn't hit anything, project a point far in front of the camera
+	if not _aim_raycast.is_colliding():
+		target_pos = _aim_raycast.global_transform.origin + -_camera.global_transform.basis.z * laser_range
+	
+	# Laser Logic
+	var eye_pos_l = _eye_l.global_position
+	var dist_l = eye_pos_l.distance_to(target_pos)
+	var direction_l = (target_pos - eye_pos_l).normalized()
+	_laser_l.global_position = eye_pos_l.lerp(target_pos, 0.5)
+	var rotation_l = Quaternion(Vector3.UP, direction_l)
+	_laser_l.global_transform.basis = Basis(rotation_l)
+	_laser_l.scale = Vector3(1, dist_l, 1)
+
+	var eye_pos_r = _eye_r.global_position
+	var dist_r = eye_pos_r.distance_to(target_pos)
+	var direction_r = (target_pos - eye_pos_r).normalized()
+	_laser_r.global_position = eye_pos_r.lerp(target_pos, 0.5)
+	var rotation_r = Quaternion(Vector3.UP, direction_r)
+	_laser_r.global_transform.basis = Basis(rotation_r)
+	_laser_r.scale = Vector3(1, dist_r, 1)
+	
+	# --- Damage Logic ---
+	if _aim_raycast.is_colliding():
+		var collider = _aim_raycast.get_collider()
+		# Check if the object we hit has a method to take damage
+		if collider and collider.has_method("take_damage"):
+			# Deal damage over time (damage value * delta)
+			collider.take_damage(laser_damage * delta)
 
 func fly():
 	if current_state == State.FLIGHT or \
@@ -480,11 +529,6 @@ func fly():
 		velocity.y = 0.0
 		_gravity = 0.0
 		can_move = true
-
-func spawn_projectile():
-	var ball = projectile_scene.instantiate()
-	ball.global_transform = %BallSpawn.global_transform
-	get_tree().root.add_child(ball)
 
 func _setup_local_player_fog():
 	# Safety check
@@ -536,24 +580,21 @@ func switch_shoulders():
 
 func make_sonic_boom():
 	sonic_boom_vfx.emitting = true
-	fx_player.play(0.0)
+	boom_sfx.play(0.0)
 
 # --- Public Functions for Modifiers ---
 
-# Call this from other scripts to set a new persistent speed modifier.
 func set_speed_modifier(value: float):
 	speed_modifier = value
 
-# Call this to reset the speed modifier to its default state.
 func reset_speed_modifier():
 	speed_modifier = 1.0
 
-# Call this to apply a temporary speed modifier (e.g., a 5-second slow debuff).
 func apply_timed_speed_modifier(value: float, duration: float):
 	set_speed_modifier(value)
 	await get_tree().create_timer(duration).timeout
 	reset_speed_modifier()
 
-
+# --- Internal Functions ---
 func _on_double_click_timer_timeout() -> void:
 	click_count = 0
